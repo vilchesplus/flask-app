53d34d9ec26ba8dbc559adb130cf1029521705b3
vilchesplus <vilchesplus@gmail.com>
2022-10-17 10:38:54 +0200

    Segundo commit de prueba....

--- api-scraper.py
+++ api-scraper.py
@@ -10,8 +10,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-
 from flask import Flask, request, render_template,jsonify
 import bs4, requests, smtplib, urllib.request, time
 from decouple import config
